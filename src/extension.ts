23c59db54667e7396ef0f5240e4132a4b7943682
ulxsth <ulxsth@gmail.com>
2024-06-29 13:50:55 +0900

    upd: activate-multi-input-mode コマンドを定義

--- src/extension.ts
+++ src/extension.ts
@@ -1,8 +1,8 @@
 import * as vscode from 'vscode';
 
 export function activate(context: vscode.ExtensionContext) {
-	const disposable = vscode.commands.registerCommand('vscode-multi-input.hogehoge', () => {
-
+	const disposable = vscode.commands.registerCommand('vscode-multi-input.activate-multi-input-mode', () => {
+		
 	});
 
 	context.subscriptions.push(disposable);
