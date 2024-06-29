d0019c81e08b2bad2be3d98f23c0dd008b8977e3
ulxsth <ulxsth@gmail.com>
2024-06-29 15:28:56 +0900

    upd: 各カーソルの位置を取得できることを確認

---
+++ out/extension.js
@@ -0,0 +1,50 @@
+"use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.activate = activate;
+exports.deactivate = deactivate;
+const vscode = __importStar(require("vscode"));
+function activate(context) {
+    const activateMultiInputMode = vscode.commands.registerCommand('vscode-multi-input.activate-multi-input-mode', () => {
+        const editor = vscode.window.activeTextEditor;
+        if (!editor) {
+            vscode.window.showErrorMessage('有効なエディタが選択されていません。');
+            return;
+        }
+        let out = "";
+        editor.selections.forEach((selection) => {
+            const selectionLine = selection.active.line;
+            const selectionCharacter = selection.active.character;
+            editor.edit((editBuilder) => {
+                // TODO: ここに挿入処理を記述
+                out += `[${selectionLine}, ${selectionCharacter}], \n`;
+            });
+        });
+        vscode.window.showInformationMessage(out);
+    });
+    context.subscriptions.push(activateMultiInputMode);
+}
+function deactivate() { }
+//# sourceMappingURL=extension.js.map
--- src/extension.ts
+++ src/extension.ts
@@ -1,11 +1,28 @@
 import * as vscode from 'vscode';
 
 export function activate(context: vscode.ExtensionContext) {
-	const disposable = vscode.commands.registerCommand('vscode-multi-input.activate-multi-input-mode', () => {
-		
-	});
+	const activateMultiInputMode = vscode.commands.registerCommand('vscode-multi-input.activate-multi-input-mode', () => {
+		const editor = vscode.window.activeTextEditor;
+		if (!editor) {
+			vscode.window.showErrorMessage('有効なエディタが選択されていません。');
+			return;
+		}
 
-	context.subscriptions.push(disposable);
+		let out = "";
+		editor.selections.forEach((selection) => {
+			const selectionLine = selection.active.line;
+			const selectionCharacter = selection.active.character;
+
+			editor.edit((editBuilder) => {
+				// TODO: ここに挿入処理を記述
+				out += `[${selectionLine}, ${selectionCharacter}], \n`;
+			});
+		});
+
+		vscode.window.showInformationMessage(out);
+  });
+
+	context.subscriptions.push(activateMultiInputMode);
 }
 
 export function deactivate() {}
